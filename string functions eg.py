51d8f708dbfcde1f9a9cc10c57c2d0b6b3d105e0
98ramya <32953562+98ramya@users.noreply.github.com>
2017-10-21 12:06:20 +0530

    Add files via upload

---
+++ string functions eg.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module5
+# Purpose:
+#
+# Author:      16cse033
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse033 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+stringvar="programmer"
+v=stringvar.count('r')
+print(v)
+v=stringvar.find('g')
+print(v)
+v=stringvar.replace('m','y')
+print(v)
