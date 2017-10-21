1d18976c8815f2d220d5a000da5cbf3209721aab
98ramya <32953562+98ramya@users.noreply.github.com>
2017-10-21 15:19:45 +0530

    Add files via upload

---
+++ conditional if else.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      16cse033
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse033 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+v=int(input("enter the age"))
+if v<20:
+  print('not eligible for voting')
+else:
+  print('eligible for voting')
