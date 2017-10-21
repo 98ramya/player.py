5a52e4b9b1c11dfe7ccc919329a8ff18bb6912fd
98ramya <32953562+98ramya@users.noreply.github.com>
2017-10-21 10:29:16 +0530

    Add files via upload

---
+++ varaiable dec with mathematical opr.py
@@ -0,0 +1,37 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      16cse033
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse033 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+a=4.5+6.7
+b=78/4
+c=45-56
+d=90**3
+e=78*67
+f=78<>89
+g=89<45
+h=67>56
+i=67%6
+j=56==56
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+
