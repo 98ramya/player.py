b933064da4fcca3d681c65676e4a39fc14cc62f7
98ramya <32953562+98ramya@users.noreply.github.com>
2017-10-21 15:47:41 +0530

    Add files via upload

---
+++ odd or even using function.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse033
+#
+# Created:     21/10/2017
+# Copyright:   (c) 16cse033 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+def function():
+    print("odd or even")
+    v=int(input("the number is"))
+    if(v%2==0):
+        print("the number is even")
+    else:
+        print("the number is odd")
+function()
+
